13ccc5fd86cdd972580af6fbce6c30e25765933f
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-12-13 22:28:55 +0100

    day1_ans1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -15,11 +15,25 @@ def aoc_day_1_ans_1(input_data: list):
     print(temp)
     
     return sum(temp)
+
+def letters_to_digit(data: str):
+    letters_to_digit_dictionary = {
+        'one' : 1, 
+        'two' : 2, 
+        'three' : 3, 
+        'four' : 4, 
+        'five' : 5, 
+        'six' : 6, 
+        'seven' : 7, 
+        'eight' : 8, 
+        'nine' : 9,
+    }
     
-### test ###
+### test part 1 ###
 data = open_file(r'D:\Python\#14_advent_of_code_2023\day_1\test_input.txt')
 print(aoc_day_1_ans_1(data))
 
+### ans part 1 ###
 data_ans_1 = open_file(r'D:\Python\#14_advent_of_code_2023\day_1\input.txt')
 print(aoc_day_1_ans_1(data_ans_1))
     
