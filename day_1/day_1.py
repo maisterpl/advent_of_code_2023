d64f0bf376e232d6cc5b08a7be95d9910ce88992
Krzysztof Kotowicz <krzysztof.kotowicz91@gmail.com>
2023-12-19 20:08:39 +0100

    day 1 part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -17,7 +17,7 @@ def aoc_day_1_ans_1(input_data: list):
     return sum(temp)
 
 def letters_to_digit(data: str):
-    letters_to_digit_dictionary = {
+    letters_to_digits_dictionary = {
         'one' : 1, 
         'two' : 2, 
         'three' : 3, 
@@ -28,6 +28,21 @@ def letters_to_digit(data: str):
         'eight' : 8, 
         'nine' : 9,
     }
+    temp: list = []
+    
+    for dig in data:
+        if dig.isdigit():
+            temp.insert(data.index(dig), dig)
+            continue
+        for let_dig in letters_to_digits_dictionary.keys():
+            if let_dig in data:
+                temp.insert(data.index(let_dig), str(letters_to_digits_dictionary[let_dig]))
+                continue
+            
+    return temp
+
+
+            
     
 ### test part 1 ###
 data = open_file(r'D:\Python\#14_advent_of_code_2023\day_1\test_input.txt')
@@ -36,4 +51,16 @@ print(aoc_day_1_ans_1(data))
 ### ans part 1 ###
 data_ans_1 = open_file(r'D:\Python\#14_advent_of_code_2023\day_1\input.txt')
 print(aoc_day_1_ans_1(data_ans_1))
+
+### test part 2 ###
+temp = letters_to_digit('26fmrrhhpthree6b')
+print(temp)
+
+# data = open_file(r'D:\Python\#14_advent_of_code_2023\day_1\test_input_part_2.txt')
+
+# temp: list = []
+# for d in data:
+#     new_data = letters_to_digit(d)
+#     temp.append(new_data)
+# print(temp)
     
